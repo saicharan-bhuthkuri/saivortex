fa48f4287004d6da93333e7f00cd1a9ed09bf58b
Fayaz Mohammad <mdfayaz9963@gmail.com>
2025-10-06 23:14:56 +0530

    text fix: rename index.html to app.html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function Home() {
     useEffect(() => {
-        window.location.href = "/index.html";
+        window.location.href = "/app.html";
     }, []);
     return null;
 }
